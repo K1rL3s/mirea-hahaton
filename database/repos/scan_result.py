527cda27b57454d75f5593ccd6dbf87f6fb478fd
K1rLes <kirill.lesovoi@mail.ru>
2024-11-23 02:59:50 +0300

    Реализация методов в репах

--- database/repos/scan_result.py
+++ database/repos/scan_result.py
@@ -1,15 +1,35 @@
 from uuid import UUID as UUID4
 
+from sqlalchemy import select
+
 from database.models.scan_result import ScanResult
 from database.repos.base import BaseAlchemyRepo
 
 
 class ScanResultRepo(BaseAlchemyRepo):
-    async def create(self, port: ScanResult) -> None:
-        pass
+    async def create(
+        self,
+        uuid: UUID4,
+        ip: str,
+        ptr_record: str,
+        severity: str,
+    ) -> ScanResult:
+        scan = ScanResult(id=uuid, ip=ip, ptr_record=ptr_record, severity=severity)
+        self.session.add(scan)
+        await self.session.commit()
+        return scan
 
-    async def get_by_uuid(self, uuid: UUID4) -> ScanResult | None:
-        pass
+    async def get_by_uuid(self, uuid: UUID4) -> list[ScanResult]:
+        query = select(ScanResult).where(ScanResult.id == uuid)
+        ips = await self.session.scalars(query)
+        return list(ips)
 
-    async def get_by_ip(self, ip: str) -> ScanResult | None:
-        pass
+    async def get_by_ip(self, ip: str) -> list[ScanResult]:
+        query = select(ScanResult).where(ScanResult.ip == ip)
+        ips = await self.session.scalars(query)
+        return list(ips)
+
+    async def get_by_ip_and_uuid(self, ip: str, uuid: UUID4) -> ScanResult | None:
+        query = select(ScanResult).where(ScanResult.ip == ip, ScanResult.id == uuid)
+        ip = await self.session.scalar(query)
+        return ip
